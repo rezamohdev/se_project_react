937262f9f7a3dee7ab7be56f67f579fcf5e4ec63
rezamohdev <mohammadireza29@gmail.com>
2023-06-17 18:30:53 -0400

    header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactDOM } from "react";
+import './Header.css';
+// import logo from "./logo.svg";
+function Header() {
+    return (
+        <>
+            <div className="header">
+                <div className="header__left-side">
+                    <img src="/images/logo.svg" alt="logo" className="header__logo" />
+                    <p className="header__date">June 15 New York</p>
+                </div>
+                <div className="header__right-side">
+                    <button className="header__button">Add clothes +</button>
+                    <p className="header__user-title">Terrence Tegegne</p>
+                    <img className="header__avatar" src="/images/avatar.svg" />
+                </div>
+            </div>
+        </>
+    );
+}
+export default Header;
