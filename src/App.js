1ba97af1b085798dd0309e085c4f4bda85bcb006
rezamohdev <mohammadireza29@gmail.com>
2023-06-18 15:45:04 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
@@ -9,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/utils/utils/constants.js
@@ -0,0 +1,105 @@
+// default clothing items array
+const defaultClothingItems = [
+    {
+        _id: 0,
+        name: "Cap",
+        weather: "hot",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
+    },
+    {
+        _id: 1,
+        name: "Hoodie",
+        weather: "warm",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
+    },
+    {
+        _id: 2,
+        name: "Jacket",
+        weather: "cold",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
+    },
+    {
+        _id: 3,
+        name: "Sneakers",
+        weather: "cold",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
+    },
+    {
+        _id: 4,
+        name: "T-Shirt",
+        weather: "hot",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
+    },
+    {
+        _id: 5,
+        name: "Winter coat",
+        weather: "cold",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
+    }
+];
+
+
+// weather conditions array
+const weatherOptions = [
+    {
+        url: require("../../../images/weather_conditions/day-cloudy.svg").default,
+        type: 'cloudy',
+        day: true
+    },
+    {
+        url: require("../../../images/weather_conditions/day-fog.svg").default,
+        type: 'fog',
+        day: true
+    },
+    {
+        url: require("../../../images/weather_conditions/day-rain.svg").default,
+        type: 'rain',
+        day: true
+    },
+    {
+        url: require("../../../images/weather_conditions/day-snow.svg").default,
+        type: 'snow',
+        day: true
+    },
+    {
+        url: require("../../../images/weather_conditions/day-storm.svg").default,
+        type: 'storm',
+        day: true
+    },
+    {
+        url: require("../../../images/weather_conditions/day-sunny.svg").default,
+        type: 'sunny',
+        day: true
+    },
+    {
+        url: require("../../../images/weather_conditions/night-cloudy.svg").default,
+        type: 'cloudy',
+        day: false
+    },
+    {
+        url: require("../../../images/weather_conditions/night-fog.svg").default,
+        type: 'fog',
+        day: false
+    },
+    {
+        url: require("../../../images/weather_conditions/night-moon.svg").default,
+        type: 'moon',
+        day: false
+    },
+    {
+        url: require("../../../images/weather_conditions/night-rain.svg").default,
+        type: 'rain',
+        day: false
+    },
+    {
+        url: require("../../../images/weather_conditions/night-snow.svg").default,
+        type: 'snow',
+        day: false
+    },
+    {
+        url: require("../../../images/weather_conditions/night-storm.svg").default,
+        type: 'storm',
+        day: false
+    },
+];
+export { defaultClothingItems, weatherOptions };
