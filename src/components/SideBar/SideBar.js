269bb3de6a77af2afb43951c906981392d1584a7
rezamohdev <mohammadireza29@gmail.com>
2023-08-21 18:50:55 -0400

    sidebar profile update

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import './SideBar.css';
 import avatarImage from '../../images/avatar.svg';
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function SideBar() {
+    const userData = React.useContext(CurrentUserContext);
+
     return (
         <div className="sidebar">
             <div className="sidebar__info">
-                <img src={avatarImage} alt="User avatar" className="sidebar__user-avatar" />
-                <p className="sidebar__user-title">John Doe</p>
+                {/* <img src={userData.avatar} alt="User avatar" className="sidebar__user-avatar" /> */}
+                {userData?.avatar ? (<img className="sidebar__user-avatar" src={userData?.avatar} alt='avatar' />) :
+                    <div className='header__avatar-placeholder'>{Array.from(userData?.name)[0].toUpperCase()}</div>}
+                <p className="sidebar__user-title">{userData.name}</p>
             </div>
         </div>
     )
