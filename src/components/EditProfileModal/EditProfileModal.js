2582711fd152f7a337520f3d70610ce9ffab1b2a
rezamohdev <mohammadireza29@gmail.com>
2023-08-22 17:14:10 -0400

    3. Add the editing profile logic

--- src/components/EditProfileModal/EditProfileModal.js
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -7,19 +7,25 @@ function EditProfileModal({ handleCloseModal, isOpen = { isOpen }, buttonText, i
     const { values, handleChange, setValues } = useForm({})
     const userData = React.useContext(CurrentUserContext);
 
-
+    React.useEffect(() => {
+        if (values && !values.name &&
+            userData?.name) {
+            setValues(userData)
+        }
+    }, [values, setValues, userData])
     const handleSubmit = (e) => {
         e.preventDefault();
-        setValues('')
+        // setValues(userData)
         onUpdateUser(values)
     }
+
     return (
         <ModalWithForm isOpen={isOpen} title="Change profile data" onClose={handleCloseModal} name='edit' buttonText={buttonText} onSubmit={handleSubmit}>
             <fieldset className='form__fieldset'>
                 <label htmlFor='name' className='form__label'>Name</label>
-                <input type='text' id='name' className='form__input' placeholder='Name' name="name" minLength={1} maxLength={30} onChange={handleChange} value={userData.name} />
+                <input type='text' id='name' className='form__input' placeholder='Name' name="name" minLength={1} maxLength={30} onChange={handleChange} value={values.name} />
                 <label htmlFor='url' className='form__label'>Avatar</label>
-                <input id='url' type='url' className='form__input' placeholder='Avatar URL' name="avatar" onChange={handleChange} value={userData.avatar} />
+                <input id='url' type='url' className='form__input' placeholder='Avatar URL' name="avatar" onChange={handleChange} value={values.avatar} />
             </fieldset>
         </ModalWithForm >
 
